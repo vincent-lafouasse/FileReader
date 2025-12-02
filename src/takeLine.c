52ccdf829d68ba3f85663f480ff06781b7753722
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-12-02 23:37:23 +0100

    sketch

--- src/takeLine.c
+++ src/takeLine.c
@@ -1,6 +1,30 @@
 #include "FileReader.h"
 
+#include <stdlib.h>
+#include <string.h>
+
+uint8_t* newStringFromSlice(const uint8_t* data, size_t size)
+{
+    if (!data || !size) {
+        return NULL;
+    }
+
+    uint8_t* out = malloc(size + 1);
+    if (!out) {
+        return NULL;
+    }
+
+    memcpy(out, data, size);
+    out[size] = 0;
+    return out;
+}
+
 AllocResult fr_takeLineAlloc(FileReader* fr)
 {
+    uint8_t* maybeNewline =
+        memchr(fr->buffer + fr->head, '\n', fr->len - fr->head);
+    if (maybeNewline) {
+        size_t len = maybeNewline + 1 - (fr->buffer + fr->head);
+    }
     return (AllocResult){0};
 }
