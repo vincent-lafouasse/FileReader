15db2594c23866df96a292537f92f0d6612e8964
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-12-03 09:24:06 +0100

    cook up a quick dynamic array for readline

--- src/takeLine.c
+++ src/takeLine.c
@@ -19,48 +19,56 @@ static uint8_t* newStringFromSlice(const uint8_t* data, size_t size)
     return out;
 }
 
+typedef struct {
+    uint8_t* data;
+    size_t len;
+    size_t cap;
+} ByteVector;
+
+static const float growingFactor = 1.5;
+static const size_t initialCapacity = 64;
+
+ReadError bv_grow(ByteVector* vec)
+{
+    if (vec->cap == 0) {
+        vec->data = malloc(initialCapacity);
+        vec->cap = initialCapacity;
+        if (!vec->data) {
+            return Read_OOM;
+        } else {
+            return Read_Ok;
+        }
+    }
+
+    const float rawCapacity = growingFactor * (float)vec->cap;
+    const size_t newCapacity = (size_t)rawCapacity;
+    uint8_t* newBuffer = malloc(newCapacity);
+    if (!newBuffer) {
+        return Read_OOM;
+    }
+    memcpy(newBuffer, vec->data, vec->len);
+    vec->cap = newCapacity;
+    free(vec->data);
+    vec->data = newBuffer;
+    return Read_Ok;
+}
+
+ReadError bv_push(ByteVector* vec, uint8_t value)
+{
+    if (vec->len == vec->cap) {
+        ReadError err = bv_grow(vec);
+        if (err != Read_Ok) {
+            return err;
+        }
+    }
+
+    vec->data[vec->len++] = value;
+    return Read_Ok;
+}
+
 AllocResult fr_takeLineAlloc(FileReader* fr)
 {
     AllocResult out = {.data = NULL, .len = 0, .err = Read_Ok};
-
-    uint8_t* head = fr->buffer + fr->head;
-    size_t remainingLen = fr->len - fr->head;
-    uint8_t* maybeNewline = memchr(head, '\n', remainingLen);
-    if (maybeNewline) {
-        size_t len = maybeNewline + 1 - head;
-        SliceResult res = fr_takeSlice(fr, len);
-        if (res.err != Read_Ok) {
-            out.err = res.err;
-            goto out;
-        }
-
-        out.data = newStringFromSlice(res.slice, len);
-        out.len = len;
-        if (!out.data) {
-            out.err = Read_OOM;
-            goto out;
-        }
-
-        goto out;
-    }
-
-    SliceResult remainingBuffer = fr_takeSlice(fr, remainingLen);
-    if (remainingBuffer.err != Read_Ok) {
-        out.err = remainingBuffer.err;
-        goto out;
-    }
-
-    uint8_t* line = newStringFromSlice(remainingBuffer.slice, remainingLen);
-    if (!line) {
-        out.err = Read_OOM;
-        goto out;
-    }
-
-    SliceResult buffer = fr_peekSlice(fr, FILE_READER_BUFFER_SIZE);
-    if (buffer.err != Read_Ok) {
-        out.err = buffer.err;
-        goto out;
-    }
 
 out:
     if (out.err != Read_Ok) {
